b836eb2400a5c2ac837258cfed8c0fbb5cda8318
Allcantaraa <kauayt120@gmail.com>
2025-02-26 12:53:54 -0300

    feat: formulario de cadastro

--- src/app/components/pages/cadastro/cadastro.component.ts
+++ src/app/components/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersList } from '../../../data/usuario-lista';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -7,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+
+  usuarios = UsersList;
+
+  formularioDeUsuario: FormGroup = new FormGroup({});
+
+  constructor(private formbuilder: FormBuilder) { }
+  
+  inicializaFormulario() {
+    this.formularioDeUsuario = this.formbuilder.group({
+      nome: ['', [Validators.required, Validators.maxLength(250)]],
+      email: ['', [Validators.email, Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      perfil: ['', [Validators.required]]
+    });
+  }
+
+
+  ngOnInit(): void {
+    this.inicializaFormulario();
+  }
+
+  SubmitForm() {
+    if (this.formularioDeUsuario.valid) {
+      this.usuarios.push(this.formularioDeUsuario.value);
+      this.formularioDeUsuario.reset();
+    }
+  }
 
 }
